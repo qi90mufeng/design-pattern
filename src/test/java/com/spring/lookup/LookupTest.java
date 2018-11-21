2c1d31b53243b462432b1f3ff228627fa28094bb
qi90mufeng <fujin@sinafenqi.com>
2018-11-21 16:59:51 +0800

    lookup

---
+++ src/test/java/com/spring/lookup/LookupTest.java
@@ -0,0 +1,50 @@
+package com.spring.lookup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * ////////////////////////////////////////////////////////////////////
+ * //                          _ooOoo_                               //
+ * //                         o8888888o                              //
+ * //                         88" . "88                              //
+ * //                         (| ^_^ |)                              //
+ * //                         O\  =  /O                              //
+ * //                      ____/`---'\____                           //
+ * //                    .'  \\|     |//  `.                         //
+ * //                   /  \\|||  :  |||//  \                        //
+ * //                  /  _||||| -:- |||||-  \                       //
+ * //                  |   | \\\  -  /// |   |                       //
+ * //                  | \_|  ''\---/''  |   |                       //
+ * //                  \  .-\__  `-`  ___/-. /                       //
+ * //                ___`. .'  /--.--\  `. . ___                     //
+ * //              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+ * //            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+ * //            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+ * //      ========`-.____`-.___\_____/___.-`____.-'========         //
+ * //                           `=---='                              //
+ * //      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+ * //         佛祖保佑       永无BUG     永不修改                      //
+ * ////////////////////////////////////////////////////////////////////
+ *
+ * @author fujin
+ * @version $Id: LookupTest.java, v 0.1 2018-09-07 Exp $$
+ */
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {ClassA.class, ClassB.class})
+public class LookupTest {
+
+    @Autowired
+    private ClassA classA;
+
+    @Test
+    public void simpleTest() {
+        for (int i = 0; i < 3; i++) {
+            classA.printClass();
+        }
+    }
+}
