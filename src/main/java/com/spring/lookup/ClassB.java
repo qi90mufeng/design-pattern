5803bf919872320b459cfb78955923f0dee0b63a
qi90mufeng <fujin@sinafenqi.com>
2018-11-21 17:05:32 +0800

    原型

---
+++ src/main/java/com/spring/lookup/ClassB.java
@@ -0,0 +1,43 @@
+package com.spring.lookup;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
+
+
+/**
+ * ////////////////////////////////////////////////////////////////////
+ * //                          _ooOoo_                               //
+ * //                         o8888888o                              //
+ * //                         88" . "88                              //
+ * //                         (| ^_^ |)                              //
+ * //                         O\  =  /O                              //
+ * //                      ____/`---'\____                           //
+ * //                    .'  \\|     |//  `.                         //
+ * //                   /  \\|||  :  |||//  \                        //
+ * //                  /  _||||| -:- |||||-  \                       //
+ * //                  |   | \\\  -  /// |   |                       //
+ * //                  | \_|  ''\---/''  |   |                       //
+ * //                  \  .-\__  `-`  ___/-. /                       //
+ * //                ___`. .'  /--.--\  `. . ___                     //
+ * //              ."" '<  `.___\_<|>_/___.'  >'"".                  //
+ * //            | | :  `- \`.;`\ _ /`;.`/ - ` : | |                 //
+ * //            \  \ `-.   \_ __\ /__ _/   .-` /  /                 //
+ * //      ========`-.____`-.___\_____/___.-`____.-'========         //
+ * //                           `=---='                              //
+ * //      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        //
+ * //         佛祖保佑       永无BUG     永不修改                      //
+ * ////////////////////////////////////////////////////////////////////
+ *
+ * @author fujin
+ * @version $Id: ClassB.java, v 0.1 2018-09-07 Exp $$
+ */
+@Component
+@Scope(value = SCOPE_PROTOTYPE)
+public class ClassB {
+
+    public void printClass() {
+        System.out.println("This is Class B: " + this);
+    }
+}
